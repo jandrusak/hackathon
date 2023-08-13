a926b9e975d6ae4bbf67c788d868ac24b48a3018
Magues17 <mikel.d.campbell@gmail.com>
2023-08-13 15:52:31 -0500

    fix the listarticles.js

--- hackathon-react-app/src/Components/ListArticles.js
+++ hackathon-react-app/src/Components/ListArticles.js
@@ -1,18 +1,30 @@
-import axios from 'axios';
-import react from 'react';
-import DisplayArticleCard from './DisplayArticleCard';
+import axios from "axios";
+import react from "react";
+import DisplayArticleCard from "./DisplayArticleCard";
 // import './ListArticles.css';
 
 function ListArticles(props) {
+  const { articles } = props;
+
+  if (!Array.isArray(articles)) {
+    return <p>No articles to display.</p>;
+  }
 
   return (
-    <div className="ListArticles">
-      {
-        props.articles.map((article, index) => {
-          return <DisplayArticleCard key={index} blank={article}/>
-        })
-      }
+    <div>
+      {props.articles.map((article) => (
+        <div key={article.objectID}>
+          <DisplayArticleCard blank={article} />
+        </div>
+      ))}
     </div>
+    // <div className="ListArticles">
+    //   {
+    //     props.articles.map((article, index) => {
+    //       return <DisplayArticleCard key={index} blank={article}/>
+    //     })
+    //   }
+    // </div>
   );
 }
 
