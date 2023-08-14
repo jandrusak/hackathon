a2d1095d84bd13564683a89f86f7de637fe91f7c
brookecowey <brooke.cowey@gmail.com>
2023-08-13 19:16:25 -0500

    updated padding

--- hackathon-react-app/src/Components/DisplayArticleCard.js
+++ hackathon-react-app/src/Components/DisplayArticleCard.js
@@ -5,13 +5,13 @@ function DisplayArticleCard(props) {
   console.log(props)
     return (
       <div className="DisplayArticleCard">
-        <span style = {{display: "flex", gap: "15px", justifyContent: "left", backgroundColor: "beige"}}>
+        <span style = {{display: "flex", gap: "15px", justifyContent: "left", backgroundColor: "beige", padding: "10px"}}>
         <a href = {`${props.blank.url}`} target = "_blank"><p>{props.blank.title}</p></a>
         </span>
-        <span style = {{display: "flex", gap: "15px", justifyContent: "left", backgroundColor: "beige"}}>
+        <span style = {{display: "flex", gap: "15px", justifyContent: "left", backgroundColor: "beige", padding: "10px"}}>
         <p> created on: {props.blank.created_at}</p>
         </span>
-        <span style = {{display: "flex", gap: "15px", justifyContent: "left", backgroundColor: "beige"}}>
+        <span style = {{display: "flex", gap: "15px", justifyContent: "left", backgroundColor: "beige", padding: "10px"}}>
         user: {props.blank.author} || {props.blank.points} points
         </span>
       </div>
